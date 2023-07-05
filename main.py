d54d01efa3c86e08853124947795daecfed3c5cc
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-07-05 14:46:03 +0900

    Feat : 회원가입 로직 기능 구현
    프론트엔드 및 백엔드 작업 (signup)

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI,UploadFile, Form, Response
+from fastapi import FastAPI, UploadFile, Form, Response
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
 
-con = sqlite3.connect('db.db', check_same_thread=False)
+con = sqlite3.connect("db.db", check_same_thread=False)
 cur = con.cursor()
 
-cur.execute(f"""
+cur.execute(
+    f"""
             CREATE TABLE IF NOT EXISTS items (
             id INTEGER PRIMARY KEY,
             title TEXT NOT NULL,
@@ -18,45 +19,73 @@ cur.execute(f"""
             place TEXT NOT NULL,
             insertAt INTEGER NOT NULL
             );
-        """)
+        """
+)
 
 app = FastAPI()
 
-@app.post('/items')
-async def create_item(image:UploadFile,
-                title:Annotated[str, Form()],
-                price:Annotated[int, Form()],
-                description:Annotated[str, Form()],
-                place:Annotated[str, Form()],
-                insertAt:Annotated[int, Form()]):
-    
+
+@app.post("/items")
+async def create_item(
+    image: UploadFile,
+    title: Annotated[str, Form()],
+    price: Annotated[int, Form()],
+    description: Annotated[str, Form()],
+    place: Annotated[str, Form()],
+    insertAt: Annotated[int, Form()],
+):
     image_bytes = await image.read()
-    cur.execute(f"""
+    cur.execute(
+        f"""
                 INSERT INTO items(title,image,price,description,place,insertAt)
                 VALUES ('{title}', '{image_bytes.hex()}', '{price}', '{description}', '{place}', '{insertAt}')
-                """)
+                """
+    )
     con.commit()
-    return 200;
+    return 200
 
-@app.get('/items')
+
+@app.get("/items")
 async def get_items():
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    rows = cur.execute(f"""
+    rows = cur.execute(
+        f"""
                        SELECT * from items
-                       """).fetchall()    
-    
+                       """
+    ).fetchall()
+
     return JSONResponse(jsonable_encoder(dict(row) for row in rows))
-    
-@app.get('/image/{item_id}')
+
+
+@app.get("/image/{item_id}")
 async def get_image(item_id):
     cur = con.cursor()
-    image_bytes = cur.execute(f"""
+    image_bytes = cur.execute(
+        f"""
                               SELECT image from items WHERE id={item_id}
-                              """).fetchone()[0]
-    
-    return Response(content=bytes.fromhex(image_bytes))
+                              """
+    ).fetchone()[0]
 
-app.mount("/", StaticFiles(directory="frontend", html = True), name="frontend")
+    return Response(content=bytes.fromhex(image_bytes), media_type="image/*")
+
+
+@app.post("/signup")
+def signup(
+    id: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    name: Annotated[str, Form()],
+    email: Annotated[str, Form()],
+):
+    cur.execute(
+        f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES('{id}', '{name}', '{email}', '{password}')
+                """
+    )
+    con.commit()
+    return 200
+
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 # 루트 path임으로 가장 아래에 위치하는게 좋음
-
